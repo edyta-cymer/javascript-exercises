f48ccf2a718e38772018b06d246045f4bec302ed
Edyta Cymer <73744715+edyta-cymer@users.noreply.github.com>
2023-09-04 14:17:04 +0200

    20. Type Conversion and Coercion
    Section 2.
    20. Type Conversion and Coercion

--- Section2/20/script.js
+++ Section2/20/script.js
@@ -1,14 +1,15 @@
 // type conversion
 const inputYear = "1991";
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
 
-console.log(Number("Jonas"));
-console.log(typeof Nan);
+// console.log(Number("Jonas"));
+// console.log(typeof Nan);
 
 // type coercion
-console.log("I am" + 25 + "years old!");
-console.log("23" - "10" - 3);
+
+// console.log("I am" + 25 + "years old!");
+// console.log("23" - "10" - 3);
 confirm.log(23 / "3");
 
 let n = 1 + "1";
