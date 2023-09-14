3dcd79e5a0b45d48e012d50806d62fb13850e4d4
Edyta Cymer <73744715+edyta-cymer@users.noreply.github.com>
2023-09-14 12:57:50 +0200

    73. Handling Click Events
    Section 7.
    JavaScript in the Browser: DOM and Events Fundamentals
    73. Handling Click Events

--- Section7/Project-1/script.js
+++ Section7/Project-1/script.js
@@ -1,8 +1,17 @@
 "use strict";
 
-document.querySelector(".message").textContent;
-document.querySelector(".message").textContent = "🎉Correct Number";
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 10;
+// document.querySelector(".message").textContent;
+// document.querySelector(".message").textContent = "🎉Correct Number";
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
 
-document.querySelector(".guess").value = 23;
+// document.querySelector(".guess").value = 23;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = " No number!";
+  }
+});
