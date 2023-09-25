6df38d34a2def02b107eb7e493471f8a79694174
Edyta Cymer <73744715+edyta-cymer@users.noreply.github.com>
2023-09-25 20:50:23 +0200

    76. Coding Challange #1
    Section 7.
    76. Coding Challange #1

--- Section7/Project-1/script.js
+++ Section7/Project-1/script.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(".number").textContent = secretNumber;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
@@ -15,9 +15,10 @@ document.querySelector(".check").addEventListener("click", function () {
     //
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
-
+    document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+
     // When guess is to high
     //
   } else if (guess > secretNumber) {
@@ -37,4 +38,13 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".score").textContent = score;
   }
 });
-// ilość kodu ćwiczebnie ; )
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  let secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
