28977fe198211fe6d4594dd2c8984d6b19b6e0a2
Edyta Cymer <73744715+edyta-cymer@users.noreply.github.com>
2023-09-25 19:34:55 +0200

    75. Manipulating CSS Styles
    Section 7
    75. Manipulating CSS Styles

--- Section7/Project-1/script.js
+++ Section7/Project-1/script.js
@@ -6,10 +6,20 @@ document.querySelector(".number").textContent = secretNumber;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
+  // When there is no input
+  //
   if (!guess) {
     document.querySelector(".message").textContent = "⛔️ No number!";
+
+    // When player wins
+    //
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    // When guess is to high
+    //
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too high!";
@@ -19,6 +29,8 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "You lost the  game!";
       document.querySelector(".score").textContent = 0;
     }
+    // When guess is to low
+    //
   } else if (guess < secretNumber) {
     document.querySelector(".message").textContent = "📉 Too low!";
     score--;
